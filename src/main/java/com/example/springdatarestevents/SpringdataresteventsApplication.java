8fb1ea509c3eb25b3fb187ff373856ddcbbedbd8
skrudra <skrudra12@gmail.com>
2019-11-20 16:12:52 +0530

    Initial commit:

---
+++ src/main/java/com/example/springdatarestevents/SpringdataresteventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatarestevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataresteventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataresteventsApplication.class, args);
+	}
+
+}
