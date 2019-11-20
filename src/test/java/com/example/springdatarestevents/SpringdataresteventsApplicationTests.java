9b29ea8d9f5b09b2064b83435a37f979651a8abc
skrudra <skrudra12@gmail.com>
2019-11-20 16:12:52 +0530

    Initial commit:

---
+++ src/test/java/com/example/springdatarestevents/SpringdataresteventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatarestevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataresteventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
