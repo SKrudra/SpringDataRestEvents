94ae032f423cfa298ca5c91e4b6961fbbe51d110
skrudra <skrudra12@gmail.com>
2019-12-05 17:59:04 +0530

    MockMvc Test

--- src/test/java/com/example/springdatarestevents/SpringDataRestEventsApplicationTests.java
+++ src/test/java/com/example/springdatarestevents/SpringDataRestEventsApplicationTests.java
@@ -1,15 +1,37 @@
 package com.example.springdatarestevents;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@WebMvcTest()
+@AutoConfigureMockMvc
 class SpringDataRestEventsApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	public void getAllBooks() throws Exception {
+		System.out.println("test mvc");
+		mvc.perform(MockMvcRequestBuilders.get("/books").accept(MediaType.APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
 	}
 
 }
